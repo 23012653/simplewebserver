35a0400ae292bcc1eb37f8016119070b0e87ccd6
23012653 <anubharathi2005@gmail.com>
2024-03-19 14:48:41 +0530

    Success

--- snmp/myapp/models.py
+++ snmp/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib import admin
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',8000)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
